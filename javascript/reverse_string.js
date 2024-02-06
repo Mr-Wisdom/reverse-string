1b0225cfd099bc6f6926406493710e68a4f70560
AndreMarrero <andremjr65@gmail.com>
2024-02-06 15:19:28 -0500

    finished algo with explanation

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -24,5 +24,7 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
-// w
 // And a written explanation of your solution
+// written explanation:
+// Because strings are iterable I used a forOf loop to iterate over each character in the string and updated a value that would be returned as the reversed string:
+// for example when you input meow(because why wouldn't you?) reversedString first gets m + its initial value which is an empty string. reversedString then equals m before which the loops runs over the next character e and then the cycle continues until the whole string is built backwords!
